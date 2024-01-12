69684138fdd9d8a6852528661b80de8694a10749
Wilson Siaw <wilsonsiawse@gmail.com>
2024-01-12 19:57:44 +0200

    ✨ feat(Application): Add filter
    Add the filter component to be rendered

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Header from './components/Header/Header'
 import SearchBar from './components/SearchBar/SearchBar'
 import Card from './components/Card/Card'
+import Filter from './components/Filter/Filter'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
     <div>
       <Header />
       <SearchBar />
+      <Filter />
       <Card />
     </div>
   )
