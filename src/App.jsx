991e332096129a98286e2735d9b4ba0842cae1a5
Wilson Siaw <wilsonsiawse@gmail.com>
2024-01-12 18:28:13 +0200

    ✨ feat(Application): Add Searchbar
    Add the Search bar component to be rendered

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/Header/Header'
+import SearchBar from './components/SearchBar/SearchBar'
 
 function App() {
 
   return (
     <div>
       <Header />
+      <SearchBar />
     </div>
   )
 }
