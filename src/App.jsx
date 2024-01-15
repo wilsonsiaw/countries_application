0232f3ae08817074313004e779ea509099cb06c9
Wilson Siaw <wilsonsiawse@gmail.com>
2024-01-15 10:34:55 +0200

    ✨ feat(Application component): Import theme
    Import the theme created from the context provider file

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,15 @@ import Header from './components/Header/Header'
 import SearchBar from './components/SearchBar/SearchBar'
 import Card from './components/Card/Card'
 import Filter from './components/Filter/Filter'
+import { ThemeContext, useTheme } from './components/context/ThemeContextProvider'
 
 function App() {
 
+  const { isDarkMode, toggleTheme } = useTheme()
+
   return (
-    <div>
-      <Header />
+    <div className='appBody'>
+      <Header handleTheme={toggleTheme}/>
       <div className="container">
         <SearchBar />
         <Filter />
