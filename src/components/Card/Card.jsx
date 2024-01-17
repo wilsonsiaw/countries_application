9728c35be18f99ab4ded98f7048d895c4d2e5b2c
Wilson Siaw <wilsonsiawse@gmail.com>
2024-01-17 22:04:15 +0200

    🦄 refactor(Card component): Add props
    Add props to the component to enable you access the data from the API

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,19 +1,18 @@
 import React from 'react'
 import './Card.css'
-import flag from '../../assets/images/german_flag.png'
 import { useTheme } from '../context/ThemeContextProvider'
 
-const Card = () => {
+const Card = (props) => {
 
   const { isDarkMode } = useTheme();
 
   return (
     <main className={`main ${isDarkMode ? 'dark-mode' : 'light-mode'}`}>
-        <img src={flag} alt="flags of countries" className='flag'/>
-        <h3>Germany</h3>
-        <h5>Population: <span>81,770,900</span></h5>
-        <h5>Region: <span>Europe</span></h5>
-        <h5>Capital: <span>Berlin</span></h5>
+        <img src={props.flag} alt="flags of countries" className='flag'/>
+        <h3>{props.name}</h3>
+        <h5>Population: <span>{props.population}</span></h5>
+        <h5>Region: <span>{props.region}</span></h5>
+        <h5>Capital: <span>{props.capital}</span></h5>
     </main>
   )
 }
