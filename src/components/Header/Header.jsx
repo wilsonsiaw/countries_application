7595999b227dce02920ba808c2fe4094c5d1db35
Wilson Siaw <wilsonsiawse@gmail.com>
2024-01-17 11:03:45 +0200

    🦄 refactor(Header component): Dark mode
    Refactor the component to make us of the dark mode theme

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import './Header.css'
 import { BsMoon } from "react-icons/bs";
+import { IoSunnyOutline } from "react-icons/io5";
+import {useTheme} from '../context/ThemeContextProvider'
 
-const Header = () => {
+const Header = ( {handleTheme} ) => {
+
+  const {isDarkMode} = useTheme();
+
   return (
-    <header className='header'>
+    <header className={`header ${isDarkMode ? 'dark-mode' : 'light-mode'}`}>
       <h1>Where in the world?</h1>
-      <div className="headerRight">
-          <BsMoon className='moon'/>
-          <h3>Dark Mode</h3>
+      <div className="headerRight" onClick={handleTheme}>
+          {isDarkMode ? <IoSunnyOutline className='moon'/> : <BsMoon className='moon'/> }
+          <h3>{isDarkMode ? 'Light Mode' : 'Dark Mode'}</h3>
       </div>
     </header>
   )
