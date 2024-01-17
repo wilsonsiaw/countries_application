314142d2e78f3947ea79263c0b87187b99dce13e
Wilson Siaw <wilsonsiawse@gmail.com>
2024-01-17 11:04:53 +0200

    🌈 style(SearchBar component): Dark mode theme
    Refactor the component to make use of the dark mode theme

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import './SearchBar.css'
-import { IoIosSearch } from "react-icons/io";
+import { IoIosSearch } from "react-icons/io"
+import { useTheme } from '../context/ThemeContextProvider'
 
 const SearchBar = () => {
+
+  const { isDarkMode } = useTheme()
+
   return (
-    <div className='searchBarWrapper'>
-        <div className="searchBar">
-            <IoIosSearch className='searchImg'/>
-            <input type="text" id='input' placeholder='Search for a country'/>
+    <div className={`searchBarWrapper ${isDarkMode ? 'dark-mode' : 'light-mode'}`}>
+        <div className={`searchBar ${isDarkMode ? 'dark-mode' : 'light-mode'}`}>
+            <IoIosSearch className={`searchImg ${isDarkMode ? 'dark-mode' : 'light-mode'}`}/>
+            <input type="text" className={`input ${isDarkMode ? 'dark-mode' : 'light-mode'}`} placeholder='Search for a country'/>
         </div>
     </div>
   )
