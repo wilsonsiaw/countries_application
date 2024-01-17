a5528018bedc918e579a6a9df8d8f36377816260
Wilson Siaw <wilsonsiawse@gmail.com>
2024-01-17 10:57:20 +0200

    🦄 refactor(Card component): Dark mode class
    Refactor the class name to use the dark mode theme

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import './Card.css'
 import flag from '../../assets/images/german_flag.png'
+import { useTheme } from '../context/ThemeContextProvider'
 
 const Card = () => {
+
+  const { isDarkMode } = useTheme();
+
   return (
-    <main className='main'>
+    <main className={`main ${isDarkMode ? 'dark-mode' : 'light-mode'}`}>
         <img src={flag} alt="flags of countries" className='flag'/>
         <h3>Germany</h3>
         <h5>Population: <span>81,770,900</span></h5>
