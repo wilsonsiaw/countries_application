4255567dd192df425ff70f93cd322857ad24360e
Wilson Siaw <wilsonsiawse@gmail.com>
2024-01-19 15:05:09 +0200

    ✨ feat(SearchBar component): Search functionality
    Add the code for the search functionality

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './SearchBar.css'
 import { IoIosSearch } from "react-icons/io"
 import { useTheme } from '../context/ThemeContextProvider'
 
-const SearchBar = () => {
+const SearchBar = ( {searchCountry, setSearchCountry, handleSearch} ) => {
 
-  const { isDarkMode } = useTheme()
+  const { isDarkMode } = useTheme();
 
   return (
-    <div className={`searchBarWrapper ${isDarkMode ? 'dark-mode' : 'light-mode'}`}>
+    <div className={`searchBarWrapper ${isDarkMode ? 'dark-mode' : 'light-mode'}`} onClick={handleSearch}>
         <div className={`searchBar ${isDarkMode ? 'dark-mode' : 'light-mode'}`}>
-            <IoIosSearch className={`searchImg ${isDarkMode ? 'dark-mode' : 'light-mode'}`}/>
-            <input type="text" className={`input ${isDarkMode ? 'dark-mode' : 'light-mode'}`} placeholder='Search for a country'/>
+            <IoIosSearch className={`searchImg ${isDarkMode ? 'dark-mode' : 'light-mode'}`} />
+            <input value={searchCountry} onChange={(e) => setSearchCountry(e.target.value)} type="text" className={`input ${isDarkMode ? 'dark-mode' : 'light-mode'}`} placeholder='Search for a country' />
         </div>
     </div>
   )
