baac11ac7730595715a76a8846edcbdf2bda5dfb
Wilson Siaw <wilsonsiawse@gmail.com>
2024-01-19 18:04:07 +0200

    ✨ feat(Home component): Create Home
    Create a home component that will be the homepage for the application. Move all code into this component

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,81 @@
+import React, {useState, useEffect} from 'react'
+import Header from '../Header/Header';
+import SearchBar from '../SearchBar/SearchBar';
+import Card from '../Card/Card';
+import Filter from '../Filter/Filter';
+import { ThemeContext, useTheme } from '../context/ThemeContextProvider';
+import axios from 'axios';
+
+const Home = () => {
+    // use dark mode and toggle from the theme
+    // context provider
+    const { isDarkMode, toggleTheme } = useTheme();
+
+    // create state to work with data from the API
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [filteredCountries, setFilteredCountries] = useState([]);
+    const [searchCountry, setSearchCountry] = useState('')
+
+    const getCountryData = async () => {
+      try {
+        setLoading(true)
+        const response = await axios.get('https://restcountries.com/v3.1/all')
+        setData(response.data);
+        setFilteredCountries(response.data)
+      } catch (error) {
+        console.error("Error fetching data:", error)
+      } finally {
+        setLoading(false)
+      }
+    };
+
+        useEffect(() => {
+            getCountryData()
+        }, []);
+
+    // create a function that searches through the cards
+    const handleSearch = () => {
+      if (searchCountry.trim() === '') {
+        setFilteredCountries(data);
+      } else {
+        const filtered = data.filter((country) => 
+        country.name.common.toLowerCase().includes(searchCountry.toLowerCase())
+        );
+        setFilteredCountries(filtered);
+      }
+    }
+
+  const cardData = filteredCountries.map(country => (
+    <Card
+      key={country.cca3} 
+      flag={country.flags.png}
+      name={country.name.common}
+      population={country.population}
+      region={country.region}
+      capital={country.capital}
+    />
+  ))
+
+  return (
+    <div>
+      <div className={`appBody ${isDarkMode ? 'dark-mode' : 'light-mode'}`}>
+      <Header handleTheme={toggleTheme}/>
+      <div className="container">
+        <SearchBar 
+          searchCountry={searchCountry}
+          setSearchCountry={setSearchCountry}
+          handleSearch={handleSearch}
+        />
+        <Filter />
+      </div>
+      <div />
+      <div className="cardContainer">
+        {cardData}
+      </div>
+    </div>
+    </div>
+  )
+}
+
+export default Home
