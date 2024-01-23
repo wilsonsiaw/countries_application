cc86c65c7ef85923307d4bd96e870da9e4269c05
Wilson Siaw <wilsonsiawse@gmail.com>
2024-01-23 18:38:07 +0200

    ✨ feat(Main): Add context
    Surround the App with the browser router context provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { ThemeContext, ThemeContextProvider } from './components/context/ThemeContextProvider.jsx'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeContextProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ThemeContextProvider>
   </React.StrictMode>,
 )
