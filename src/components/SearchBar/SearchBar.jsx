878ab38159331b3047e62055b20a50a17c243856
Wilson Siaw <wilsonsiawse@gmail.com>
2024-01-23 23:05:41 +0200

    🦄 refactor(SearchBar component): handleChange
    Refactor the handle change function to handle the event for both search functions

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -7,11 +7,17 @@ const SearchBar = ( {searchCountry, setSearchCountry, handleSearch} ) => {
 
   const { isDarkMode } = useTheme();
 
+  const handleChange = (e) => {
+    const input = e.target.value;
+    setSearchCountry(input)
+    handleSearch(input)
+  } 
+
   return (
-    <div className={`searchBarWrapper ${isDarkMode ? 'dark-mode' : 'light-mode'}`} onClick={handleSearch}>
+    <div className={`searchBarWrapper ${isDarkMode ? 'dark-mode' : 'light-mode'}`}>
         <div className={`searchBar ${isDarkMode ? 'dark-mode' : 'light-mode'}`}>
             <IoIosSearch className={`searchImg ${isDarkMode ? 'dark-mode' : 'light-mode'}`} />
-            <input value={searchCountry} onChange={(e) => setSearchCountry(e.target.value)} type="text" className={`input ${isDarkMode ? 'dark-mode' : 'light-mode'}`} placeholder='Search for a country' />
+            <input value={searchCountry} onChange={handleChange} type="text" className={`input ${isDarkMode ? 'dark-mode' : 'light-mode'}`} placeholder='Search for a country' />
         </div>
     </div>
   )
