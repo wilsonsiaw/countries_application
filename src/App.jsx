c0b06423deb2a4580c0202e1689877d0ee5ba258
Wilson Siaw <wilsonsiawse@gmail.com>
2024-01-23 18:37:17 +0200

    ✨ feat(App component): Create routes
    Create routes for move onto different pages in the application

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import { useState, useEffect } from 'react'
 import './App.css'
 import Home from './components/Home/Home'
 import Page from './components/Page/Page'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
     <div >
-      {/* <Home /> */}
-      <Page />
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/card/:id/*' element={<Page />}/>
+      </Routes>
     </div>
   )
 }
