7885af90656be540a52151fd6cf761ce5535e2b3
Wilson Siaw <wilsonsiawse@gmail.com>
2024-01-23 23:50:47 +0200

    🦄 refactor(Filter component): Change option
    An option in the select menu was not spelled correclty hence it was not filtering

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -21,7 +21,7 @@ const Filter = ( {onFilterChange} ) => {
     >
         <option value="">Filter by Region</option>
         <option value="africa">Africa</option>
-        <option value="america">America</option>
+        <option value="americas">Americas</option>
         <option value="asia">Asia</option>
         <option value="europe">Europe</option>
         <option value="oceania">Oceania</option>
