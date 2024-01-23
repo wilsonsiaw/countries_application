f858e8552814212e74ba0ef4cb29a7e6f6067f54
Wilson Siaw <wilsonsiawse@gmail.com>
2024-01-23 22:03:04 +0200

    ✨ feat(Card component): Change format
    Change the formatting of the population number

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -12,7 +12,7 @@ const Card = (props) => {
         <Link to={`/card/${props.id}`} className='card'>
             <img src={props.flag} alt="flags of countries" className='flag'/>
             <h3>{props.name}</h3>
-            <h5>Population: <span>{props.population}</span></h5>
+            <h5>Population: <span>{new Intl.NumberFormat().format(props.population)}</span></h5>
             <h5>Region: <span>{props.region}</span></h5>
             <h5>Capital: <span>{props.capital}</span></h5>
         </Link>
